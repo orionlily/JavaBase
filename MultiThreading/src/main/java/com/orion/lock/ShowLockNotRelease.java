dd2d16f3362b4598aabea2d6917067dab44a1dea
orionlily <gdoullc@163.com>
2022-03-19 11:25:53 +0800

    ReentrantLock 不释放锁导致的后果

--- MultiThreading/src/main/java/com/orion/lock/ShowLockNotRelease.java
+++ MultiThreading/src/main/java/com/orion/lock/ShowLockNotRelease.java
@@ -9,7 +9,7 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * ReentrantLock 不释放锁导致的后果
  *
- * 可见被打断后，没有释放锁，state补位0，后面在finally新起的线程拿不到锁执行不下去,只有先unlock才能执行下去
+ * 可见被打断后，没有释放锁，state不为0，后面在finally新起的线程拿不到锁执行不下去,只有先unlock才能执行下去
  *
  * @author Administrator
  */
@@ -29,12 +29,12 @@ public class ShowLockNotRelease {
                 //reentrantLock.unlock();
                 new Thread(() -> {
                     SmallTool.printTimeAndThread("r begin");
-                    reentrantLock.lock();
+                    //reentrantLock.lock(); //注释能使程序终结，lock的计数归零
                     SmallTool.printTimeAndThread("r biz");
                     reentrantLock.unlock();
-                }).start();
+                },"r2").start();
             }
-        });
+        },"r");
 
         Thread r1 = new Thread(() -> {
             reentrantLock.lock();
@@ -47,7 +47,7 @@ public class ShowLockNotRelease {
                 SmallTool.printTimeAndThread("r1 release lock");
                 reentrantLock.unlock();
             }
-        });
+        },"r1");
         r.start();
         r1.start();
 
@@ -58,15 +58,24 @@ public class ShowLockNotRelease {
     }
     /**
      * 运行结果：
-     1644473401209	|	1	|	main	|	main will interrupt
-     1644473403007	|	14	|	Thread-1	|	r1 biz
+     1647659806410	|	1	|	main	|	main will interrupt
      java.lang.InterruptedException
-     1644473403007	|	14	|	Thread-1	|	r1 release lock
-     1644473403010	|	16	|	Thread-2	|	r begin
+     1647659808220	|	12	|	r1	|	r1 biz
      at java.util.concurrent.locks.AbstractQueuedSynchronizer$ConditionObject.reportInterruptAfterWait(AbstractQueuedSynchronizer.java:2014)
+     1647659808221	|	12	|	r1	|	r1 release lock
+     1647659808223	|	14	|	r2	|	r begin
      at java.util.concurrent.locks.AbstractQueuedSynchronizer$ConditionObject.await(AbstractQueuedSynchronizer.java:2048)
-     at com.orion.lock.ShowLockNotRelease.lambda$main$1(ShowLockNotRelease.java:23)
+     1647659808223	|	14	|	r2	|	r biz
+     at com.orion.lock.ShowLockNotRelease.lambda$main$1(ShowLockNotRelease.java:25)
      at java.lang.Thread.run(Thread.java:748)
+     Exception in thread "r2" java.lang.IllegalMonitorStateException
+     at java.util.concurrent.locks.ReentrantLock$Sync.tryRelease(ReentrantLock.java:151)
+     at java.util.concurrent.locks.AbstractQueuedSynchronizer.release(AbstractQueuedSynchronizer.java:1261)
+     at java.util.concurrent.locks.ReentrantLock.unlock(ReentrantLock.java:457)
+     at com.orion.lock.ShowLockNotRelease.lambda$null$0(ShowLockNotRelease.java:34)
+     at java.lang.Thread.run(Thread.java:748)
+
+     Process finished with exit code 0
 
      */
 }
